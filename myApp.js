616d5d05d53475ef04c6837ab5fe3dbfc16a0901
damispe <damianspescha@gmail.com>
2022-08-15 02:14:19 -0300

    push09

--- myApp.js
+++ myApp.js
@@ -25,6 +25,14 @@ app.use(helmet.dnsPrefetchControl());
 
 app.use(helmet.noCache());
 
+app.use(helmet.contentSecurityPolicy());
+
+app.use(helmet.contentSecurityPolicy({ 
+  directives: { 
+    defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] 
+  }
+}));
+
 
 
 
